52fe66a997ac702c5c89a5871e74f41aacbf97b3
sade-boy <sade_24@alu.uabcs.mx>
2026-02-20 10:57:47 -0700

    login

--- src/actividad1/Ventana.java
+++ src/actividad1/Ventana.java
@@ -1,5 +1,64 @@
 package actividad1;
 
-public class Ventana {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Ventana extends JFrame{
+
+	public Ventana() {
+				
+				this.setVisible(true);
+				this.setSize(500,500);
+				this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				this.setLocationRelativeTo(null);
+				this.setMinimumSize(new Dimension(200,200));
+				this.setMaximumSize(new Dimension(600,600));
+				this.setTitle("PRUEBA");
+				this.setLayout(null);
+			
+				JPanel contenedor = new JPanel();
+				contenedor.setOpaque(true);
+				contenedor.setBackground(Color.BLACK);
+				contenedor.setSize(500,500);
+				contenedor.setLayout(null);
+				
+				this.add(contenedor);
+				
+				JLabel title_login = new JLabel();
+				title_login.setText("Bienvenido");
+				title_login.setSize(200,30);
+				title_login.setOpaque(true);
+				title_login.setLocation(150,10);
+				title_login.setBackground(Color.white);
+				title_login.setFont(new Font("Arial",Font.BOLD,22));
+				title_login.setHorizontalAlignment(JLabel.CENTER);
+				contenedor.add(title_login);
+				
+				JTextField username = new JTextField();
+				username.setSize(300,30);
+				username.setLocation(30,150);
+				username.setFont(new Font("Arial",Font.BOLD,22));
+				contenedor.add(username);
+				
+				JButton acceder = new JButton();
+				acceder.setText("acceder");
+				acceder.setSize(120,50);
+				acceder.setLocation(200,400);
+				acceder.setFont(new Font("Arial",Font.BOLD,22));
+				contenedor.add(acceder);
+				
+				
+				contenedor.repaint();
+				contenedor.revalidate();
+				
+	}
 }
+	 
+
